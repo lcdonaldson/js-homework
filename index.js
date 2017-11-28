98ece48941883fd6a29ce0413d8f1a6cd5573905
Levi Donaldson <lcdbacanas@gmail.com>
2017-11-27 21:44:06 -0600

    js interview question 1 added.

---
+++ index.js
@@ -0,0 +1,8 @@
+Interview Question Examples.
+
+# 1 return a string in reverse order. ex( hello == olleh )
+
+function reverseString(exstr) {
+    return exstr.split("").reverse().join("");
+}
+reverseString("hey guys this reversed");
